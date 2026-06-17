9b9cad4cf42ab28a02fd0dd18f885156e71c6f2a
arunoki-std <arunoki.studio@gmail.com>
2026-06-17 12:20:25 +0300

    +

--- Runtime/Interfaces/IFlowHandler.cs
+++ Runtime/Interfaces/IFlowHandler.cs
@@ -1,9 +1,16 @@
 namespace Arunoki.Flow
 {
+  /// <summary>
+  /// Marks a receiver that should be subscribed to a local flow hub.
+  /// </summary>
   public interface IFlowHandler
   {
   }
 
+  /// <summary>
+  /// Marks a receiver that should be subscribed to the global flow hub
+  /// by higher-level infrastructure.
+  /// </summary>
   public interface IFlowGlobalHandler : IFlowHandler
   {
   }
